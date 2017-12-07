66a8aa4ba2735db6911d2b2ea890bc43dd43ab8e
Björn <frangiz@gmail.com>
2017-12-07 22:49:22 +0100

    Revisited day07 and used treelib instead of building my own tree class.

--- days/day07.py
+++ days/day07.py
@@ -1,117 +1,82 @@
+from collections import Counter, deque
+from treelib import Tree
 import re
-from collections import namedtuple
-
-Node = namedtuple('Node', 'base_weight total_weight children')
 
 
-class Tree:
-    def __init__(self):
-        self.nodes = {}
-        self.root = None
+def build_tree(puzzle_input):
+    # parse input into links
+    links = {}
+    for line in puzzle_input:
+        m = re.search('([a-z]+) \((\d+)\)', line)
+        if m is not None:
+            name = m.group(1)
+            weight = m.group(2)
+            children = []
+            parts = line.split(' -> ')
+            if len(parts) > 1:
+                for child in parts[1].split(','):
+                    children.append(child.strip())
+            links[name] = (int(weight), children)
+    # find the root
+    potential_roots = set(links.keys())
+    for k, v in links.items():
+        for child in v[1]:
+            potential_roots.discard(child)
+    assert len(potential_roots) == 1
+    root = potential_roots.pop()
 
-    def build(self, puzzle_input):
-        for line in puzzle_input:
-            m = re.search('([a-z]+) \((\d+)\)', line)
-            if m is not None:
-                name = m.group(1)
-                weight = m.group(2)
-                children = []
-                parts = line.split(' -> ')
-                if len(parts) > 1:
-                    for child in parts[1].split(','):
-                        children.append(child.strip())
-                self.nodes[name] = Node(int(weight), 0, children)
-        self._update_root()
+    # build the tree
+    tree = Tree()
+    tree.create_node(root, root, data=links[root][0])
+    children = [(root, c) for c in links[root][1]]
+    while children:
+        parent, node = children.pop()
+        tree.create_node(node, node, parent=parent, data=links[node][0])
+        for child in links[node][1]:
+            children.append((node, child))
+    return tree
 
-    def find_unbalanced(self):
-        self._update_total_weights()
 
-        leaves = [k for k, v in self.nodes.items() if len(v.children) == 0]
-        layer = set([self._get_parent(n) for n in leaves])
+def calc_new_weight_for_unbalanced_node(tree):
+    leaves = [n for n in tree.all_nodes() if n.is_leaf()]
+    total_weights = {n.tag: n.data for n in leaves}
+    unvisited = deque(set(tree.parent(n.tag).tag for n in leaves))
+    while unvisited:
+        tag = unvisited.popleft()
+        children = tree.children(tag)
+        # check if we have visited all children
+        if not all(child.tag in total_weights for child in children):
+            unvisited.append(tag)
+            continue
+        # check if done
+        child_weights = [total_weights[c.tag] for c in children]
+        # true if we found the parent for the unbalanced node
+        if len(set(child_weights)) > 1:
+            c = Counter(child_weights)
+            least_common = c.most_common()[-1][0]
+            most_common = c.most_common()[0][0]
+            for c in children:
+                if total_weights[c.tag] == least_common:
+                    return c.data + most_common - least_common
 
-        unbalanced_state = None
-        while not unbalanced_state:
-            parents = set()
-            for node in layer:
-                parent = self._get_parent(node)
-                parents.add(parent)
-            for parent in parents:
-                vals = {}
-                for child in self.nodes[parent].children:
-                    if self.nodes[child].total_weight not in vals:
-                       vals[self.nodes[child].total_weight] = []
-                    vals[self.nodes[child].total_weight].append(child)
-                if len(vals) > 1:
-                    unbalanced_state = vals
-        # calc diff
-        value, node = [(k, v) for k, v in unbalanced_state.items() if len(v) == 1][0]
-        most_common_values = [k for k, v in unbalanced_state.items() if len(v) > 1][0]
-        off = value - most_common_values
+        # nope, carry on...
+        total_weight = tree.get_node(tag).data
+        total_weight += sum(total_weights[c.tag] for c in children)
+        total_weights[tag] = total_weight
+        parent_tag = tree.parent(tag).tag
+        if parent_tag is not None and parent_tag not in unvisited:
+            unvisited.append(parent_tag)
+    return 0
 
-        return self.nodes[node[0]].base_weight - off
-
-    def _update_root(self):
-        all_childrens = set()
-        potential_roots = set()
-        for name, data in self.nodes.items():
-            potential_roots.add(name)
-            for child in data.children:
-                all_childrens.add(child)
-        potential_roots = potential_roots - all_childrens
-        assert len(potential_roots) == 1
-        self.root = potential_roots.pop()
-
-    def _update_total_weights(self):
-        root = self.nodes[self.root]
-        self.nodes[self.root] = Node(
-            root.base_weight,
-            self._get_total_weight(self.root),
-            root.children)
-
-    def _get_total_weight(self, name):
-        if len(self.nodes[name].children) == 0:
-            return self.nodes[name].base_weight
-        tot = self.nodes[name].base_weight
-        for child in self.nodes[name].children:
-            w = self._get_total_weight(child)
-            self.nodes[child] = Node(self.nodes[child].base_weight, w, self.nodes[child].children)
-            tot += w
-        return tot
-
-    def _get_parent(self, child):
-        for k, v in self.nodes.items():
-            if child in v.children:
-                return k
-        return ''
-        
 
 def part_a(puzzle_input):
-    tree = Tree()
-    tree.build(puzzle_input)
-
+    tree = build_tree(puzzle_input)
     return tree.root
 
 
-def check(layer):
-    parents = set()
-    for l in layer:
-        parent = get_parent(tree, l)
-        parents.add(parent)
-        vals = {}
-        for c in tree[parent][1]:
-            if tree[c][0] not in vals:
-                vals[tree[c][0]] = []
-            vals[tree[c][0]].append(c)
-        if len(vals) > 1:
-            print('PART2: ')
-            print(vals)
-    return parents
-
 def part_b(puzzle_input):
-    tree = Tree()
-    tree.build(puzzle_input)
-    unbalanced = tree.find_unbalanced()
-    return str(unbalanced)
+    tree = build_tree(puzzle_input)
+    return str(calc_new_weight_for_unbalanced_node(tree))
 
 
 def solve(puzzle_input):
