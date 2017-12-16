c62347e0409a5223d2646e493c5f7357106fc18e
Björn <frangiz@gmail.com>
2017-12-16 07:11:30 +0100

    Improved the speed for day 15.

--- days/day15.py
+++ days/day15.py
@@ -1,37 +1,31 @@
 def part_a(puzzle_input):
-    values = []
-    for line in puzzle_input:
-        values.append(int(line.split()[4]))
-    factors = [16807, 48271]
+    val_a, val_b = [int(line.split()[4]) for line in puzzle_input]
+    factor_a, factor_b = [16807, 48271]
     matches = 0
     mask = (1 << 16) - 1
     for _ in range(40000000):
-        values[0] = (values[0] * factors[0]) % 2147483647
-        values[1] = (values[1] * factors[1]) % 2147483647
-        if (values[0] & mask) == (values[1] & mask):
+        val_a = (val_a * factor_a) % 2147483647
+        val_b = (val_b * factor_b) % 2147483647
+        if val_a & mask == val_b & mask:
             matches += 1
     return str(matches)
 
 
 def part_b(puzzle_input):
-    values = []
-    for line in puzzle_input:
-        values.append(int(line.split()[4]))
-    factors = [16807, 48271]
-    compa = []
-    compb = []
-    while (len(compa) < 5000000 or len(compb) < 5000000):
-        values[0] = (values[0] * factors[0]) % 2147483647
-        values[1] = (values[1] * factors[1]) % 2147483647
-        if values[0] % 4 == 0:
-            compa.append(values[0])
-        if values[1] % 8 == 0:
-            compb.append(values[1])
-
+    val_a, val_b = [int(line.split()[4]) for line in puzzle_input]
+    factor_a, factor_b = [16807, 48271]
     matches = 0
     mask = (1 << 16) - 1
-    for i in range(5000000):
-        if (compa[i] & mask) == (compb[i] & mask):
+    for _ in range(5000000):
+        comp_a = True
+        comp_b = True
+        while comp_a:
+            val_a = (val_a * factor_a) % 2147483647
+            comp_a = val_a % 4 != 0
+        while comp_b:
+            val_b = (val_b * factor_b) % 2147483647
+            comp_b = val_b % 8 != 0
+        if val_a & mask == val_b & mask:
             matches += 1
     return str(matches)
 
