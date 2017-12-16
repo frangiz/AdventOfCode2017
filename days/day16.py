fe8460b7876376bf9950c8596634675ba53976ae
Björn <frangiz@gmail.com>
2017-12-16 11:38:47 +0100

    Added some docstrings for day16.

--- days/day16.py
+++ days/day16.py
@@ -1,46 +1,93 @@
-def dance_once(puzzle_input, registers):
+"""--- Day 16: Permutation Promenade ---"""
+
+
+def dance_once(puzzle_input, programs):
+    """
+    Do one full round of the dance according to the puzzle_input.
+
+    Args:
+        puzzle_input (list): Formatted as the provided input from the website.
+        programs (list): The programs that will dance.
+    Returns:
+        list: The new order of the programs.
+    Examples:
+        >>> dance_once(['s1,x3/4,pe/b'], [c for c in 'abcde'])
+        ['b', 'a', 'e', 'd', 'c']
+
+    """
     for dance_move in ''.join(puzzle_input).split(','):
         if dance_move.startswith('s'):
             spin = int(dance_move[1:])
-            registers = registers[-spin:] + registers[:-spin]
+            programs = programs[-spin:] + programs[:-spin]
         elif dance_move.startswith('x'):
             a, b = map(int, dance_move[1:].split('/'))
-            registers[a], registers[b] = registers[b], registers[a]
+            programs[a], programs[b] = programs[b], programs[a]
         elif dance_move.startswith('p'):
             a, b = dance_move[1:].split('/')
-            index_a = registers.index(a)
-            index_b = registers.index(b)
-            registers[index_a], registers[index_b] = registers[index_b], registers[index_a]
-    return registers
+            index_a = programs.index(a)
+            index_b = programs.index(b)
+            programs[index_a], programs[index_b] = programs[index_b], programs[index_a]
+    return programs
 
 
-def promenade(puzzle_input, registers, iterations):
+def dance(puzzle_input, programs, iterations):
+    """
+    Dancing the programs the number of iterations. Can detect loops!
+
+    Args:
+        puzzle_input (list): Formatted as the provided input from the website.
+        programs (list): The programs that will dance.
+        iterations (int): The number of iterations to dance.
+    Returns:
+        string: The result of dancing n number of iterations.
+
+    """
     visited_states = []
     index = 0
     while index < iterations:
         # A state that we already visited indicates that we have a loop.
-        state = ''.join(registers)
+        state = ''.join(programs)
         if state in visited_states:
             state_index = visited_states.index(state)
             loop_length = len(visited_states) - state_index
             offset = (iterations - index) % loop_length
+            print(offset)
             return visited_states[state_index + offset]
         visited_states.append(state)
         # Let's dance!
-        registers = dance_once(puzzle_input, registers)
+        programs = dance_once(puzzle_input, programs)
         index += 1
-    return ''.join(registers)
+    return ''.join(programs)
 
 
 def part_a(puzzle_input):
+    """
+    Calculate the answer for part_a.
+
+    Args:
+        puzzle_input (list): Formatted as the provided input from the website.
+    Returns:
+        string: The answer for part_a.
+
+    """
     registers = [c for c in 'abcdefghijklmnop']
-    return promenade(puzzle_input, registers, 1)
+    return dance(puzzle_input, registers, 1)
 
 
 def part_b(puzzle_input):
+    """
+    Calculate the answer for part_b.
+
+    Args:
+        puzzle_input (list): Formatted as the provided input from the website.
+    Returns:
+        string: The answer for part_b.
+
+    """
     registers = [c for c in 'abcdefghijklmnop']
-    return promenade(puzzle_input, registers, 1000000000)
+    return dance(puzzle_input, registers, 1000000000)
 
 
 def solve(puzzle_input):
+    """Returs the answer for both parts."""
     return {'a': part_a(puzzle_input), 'b': part_b(puzzle_input)}
--- test/test_day16.py
+++ test/test_day16.py
@@ -1,3 +1,4 @@
+"""The tests for day16."""
 from days import day16
 from ddt import ddt, data, unpack
 import unittest
@@ -5,28 +6,28 @@ import util
 
 
 @ddt
-class MyTestCase(unittest.TestCase):
+class MyTestCase(unittest.TestCase): # noqa D101
     @data(
         [['s1'], 'eabcd'],
         [['s1,x3/4'], 'eabdc'],
         [['s1,x3/4,pe/b'], 'baedc'])
     @unpack
-    def test_example_a(self, test_input, expected):
-        registers = [c for c in 'abcde']
-        result = day16.promenade(test_input, registers, 1)
+    def test_example_a(self, test_input, expected): # noqa D102
+        programs = [c for c in 'abcde']
+        result = day16.dance(test_input, programs, 1)
         self.assertEqual(result, expected)
 
-    def test_answer_part_a(self):
+    def test_answer_part_a(self): # noqa D102
         result = day16.part_a(util.get_file_contents('day16.txt'))
         self.assertEqual(result, 'lgpkniodmjacfbeh')
 
     @data([['s1,x3/4,pe/b'], 'ceadb'])
     @unpack
-    def test_example_b(self, test_input, expected):
-        registers = [c for c in 'abcde']
-        result = day16.promenade(test_input, registers, 2)
+    def test_example_b(self, test_input, expected): # noqa D102
+        programs = [c for c in 'abcde']
+        result = day16.dance(test_input, programs, 2)
         self.assertEqual(result, expected)
 
-    def test_answer_part_b(self):
+    def test_answer_part_b(self): # noqa D102
         result = day16.part_b(util.get_file_contents('day16.txt'))
         self.assertEqual(result, 'hklecbpnjigoafmd')
